66b36e6a20c33118bb28dcef4348b956f7bf7db5
deon86116 <deon86116@gmail.com>
2020-05-20 18:25:10 +0800

    aa

--- 10_2_MQTT_Python/main.cpp
+++ 10_2_MQTT_Python/main.cpp
@@ -2,7 +2,62 @@
 #include "MQTTNetwork.h"
 #include "MQTTmbed.h"
 #include "MQTTClient.h"
+#include "fsl_port.h"
 
+#include "fsl_gpio.h"
+
+
+#define UINT14_MAX        16383
+
+// FXOS8700CQ I2C address
+
+#define FXOS8700CQ_SLAVE_ADDR0 (0x1E<<1) // with pins SA0=0, SA1=0
+
+#define FXOS8700CQ_SLAVE_ADDR1 (0x1D<<1) // with pins SA0=1, SA1=0
+
+#define FXOS8700CQ_SLAVE_ADDR2 (0x1C<<1) // with pins SA0=0, SA1=1
+
+#define FXOS8700CQ_SLAVE_ADDR3 (0x1F<<1) // with pins SA0=1, SA1=1
+
+// FXOS8700CQ internal register addresses
+
+#define FXOS8700Q_STATUS 0x00
+
+#define FXOS8700Q_OUT_X_MSB 0x01
+
+#define FXOS8700Q_OUT_Y_MSB 0x03
+
+#define FXOS8700Q_OUT_Z_MSB 0x05
+
+#define FXOS8700Q_M_OUT_X_MSB 0x33
+
+#define FXOS8700Q_M_OUT_Y_MSB 0x35
+
+#define FXOS8700Q_M_OUT_Z_MSB 0x37
+
+#define FXOS8700Q_WHOAMI 0x0D
+
+#define FXOS8700Q_XYZ_DATA_CFG 0x0E
+
+#define FXOS8700Q_CTRL_REG1 0x2A
+
+#define FXOS8700Q_M_CTRL_REG1 0x5B
+
+#define FXOS8700Q_M_CTRL_REG2 0x5C
+
+#define FXOS8700Q_WHOAMI_VAL 0xC7
+
+
+I2C i2c( PTD9,PTD8);
+
+Serial pc(USBTX, USBRX);
+
+int m_addr = FXOS8700CQ_SLAVE_ADDR1;
+
+
+void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len);
+
+void FXOS8700CQ_writeRegs(uint8_t * data, int len);
 // GLOBAL VARIABLES
 WiFiInterface *wifi;
 InterruptIn butt(SW2);
@@ -46,7 +101,7 @@ int main() {
    MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
 
    //TODO: revise host to your ip
-   const char* host = "192.168.0.148";
+   const char* host = "172.20.10.3";
    const char* topic = "Mbed";
    printf("Connecting to TCP network...\r\n");
    int rc = mqttNetwork.connect(host, 1883);
@@ -68,13 +123,65 @@ int main() {
    }
 
    butt.rise(&publish_message);
+   uint8_t who_am_i, D[2], res[6];
+
+   int16_t acc16;
+
+   float t[3];
+
+
+   // Enable the FXOS8700Q
+
+
+   FXOS8700CQ_readRegs( FXOS8700Q_CTRL_REG1, &D[1], 1);
+
+   D[1] |= 0x01;
+
+   D[0] = FXOS8700Q_CTRL_REG1;
+
+   FXOS8700CQ_writeRegs(D, 2);
+
 
    while (true) {
       if (publish) {
+            FXOS8700CQ_readRegs(FXOS8700Q_OUT_X_MSB, res, 6);
+
+
+            acc16 = (res[0] << 6) | (res[1] >> 2);
+
+            if (acc16 > UINT14_MAX/2)
+
+               acc16 -= UINT14_MAX;
+
+            t[0] = ((float)acc16) / 4096.0f;
+
+
+            acc16 = (res[2] << 6) | (res[3] >> 2);
+
+            if (acc16 > UINT14_MAX/2)
+
+               acc16 -= UINT14_MAX;
+
+            t[1] = ((float)acc16) / 4096.0f;
+
+
+            acc16 = (res[4] << 6) | (res[5] >> 2);
+
+            if (acc16 > UINT14_MAX/2)
+
+               acc16 -= UINT14_MAX;
+
+            t[2] = ((float)acc16) / 4096.0f;
+
+
+
+
+            
             message_num++;
             MQTT::Message message;
             char buff[100];
-            sprintf(buff, "QoS0 Hello, Python! #%d", message_num);
+            sprintf(buff,"FXOS8700Q ACC: X=%1.4f Y=%1.4f Z=%1.4f\r\n",t[0],t[1],t[2]);
+
             message.qos = MQTT::QOS0;
             message.retained = false;
             message.dup = false;
@@ -85,7 +192,24 @@ int main() {
             printf("rc:  %d\r\n", rc);
             printf("Puslish message: %s\r\n", buff);
             client.yield(10);
-            publish = false;
+            wait(0.5);
+
       }
    }
+}
+void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len) {
+
+   char t = addr;
+
+   i2c.write(m_addr, &t, 1, true);
+
+   i2c.read(m_addr, (char *)data, len);
+
+}
+
+
+void FXOS8700CQ_writeRegs(uint8_t * data, int len) {
+
+   i2c.write(m_addr, (char *)data, len);
+
 }
